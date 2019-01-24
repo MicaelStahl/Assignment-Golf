01ae8de07139213f7722fb0c7a8e9056fb3b3e2a
micael Ståhl <micael_stahl96@hotmail.com>
2019-01-24 16:29:45 +0100

    Stuck on a problem on the loop.

--- Assignment Golf/Program.cs
+++ Assignment Golf/Program.cs
@@ -8,79 +8,85 @@ namespace Assignment_Golf
     {
         static void Main(string[] args)
         {
-            //Math.Abs is a math formula that will always give you back a positive value
-            //Math.Round is a math formula that rounds the amount of decimals
-
-            //ballDis = Math.Abs(ballDis);
 
             SwingLoop();
 
-            //double ballDis = SwingLoop();
-            //Console.WriteLine(ballDis);
-            //Console.ReadKey();
         }
-        //static double DistanceCalculation()
-        //{
-        ////     < Summary > //
-        ////     This method calculates the distance flown by the ball after
-        ////     the player has input correct angle and velocity values
 
-        //    double distanceToHole = DistanceToHole();
-        //    double angle = AllowAngleAmount();
-        //    double velocity = AllowVelocityAmount();
-
-        //    double gravity = 9.8;
-
-        //    double radianValue = (Math.PI / 180) * angle;
-        //    double ballDistance = Math.Pow(velocity, 2) / gravity * Math.Sin(2 * radianValue);
-
-        //    double distanceBetween = distanceToHole - ballDistance;
-
-
-        //    SwingLoop(distanceBetween);
-
-        //    return Math.Round(ballDistance, 2);
-        //}
         static void SwingLoop()
         {
+            // <Summary> //
+            // This method handles the majority of the code
+            // it gathers data from various methods to 
+            // make the golfgame a possibility
+
             bool stayAlive = true;
+
             double swings = 0;
             double gravity = 9.8;
             double distanceBetween = 0;
+            //double distanceBetweenNegative = 0;
             double finalBallDistance = 0;
+            double eachSwingDistanceCount = 0;
             double distanceToHole = 0;
 
             List<double> eachSwingDistance = new List<double>();
 
+            // This calls for a method that will either let the
+            // user pick the distance to the hole, or it'll random it
             distanceToHole = DistanceToHole();
 
             while (stayAlive)
             {
+                // This While loop makes the golfgame a reality, and it starts
+                // by fetching the values for the angle and velocity to create
+                // the correct ballDistance result.
+
                 double angle = AllowAngleAmount();
                 double velocity = AllowVelocityAmount();
-                
 
 
                 double radianValue = (Math.PI / 180) * angle;
                 double ballDistance = Math.Pow(velocity, 2) / gravity * Math.Sin(2 * radianValue);
 
-                eachSwingDistance.Add(ballDistance); // Remember. make a foreach with list to show the "history" of the swings and distance on each swing
+                eachSwingDistance.Add(ballDistance); // Adds every new swing to the list.
+                eachSwingDistanceCount = eachSwingDistance.Count;
 
-                finalBallDistance = finalBallDistance + ballDistance;
+                foreach (double distance in eachSwingDistance)
+                {
+                    finalBallDistance = finalBallDistance + distance;
 
-                distanceBetween = distanceToHole - finalBallDistance;
+                    distanceBetween = distanceToHole - finalBallDistance;
 
-                if (distanceBetween < -500)
+                }
+
+                finalBallDistance = 0;
+
+                //distanceBetween = Math.Abs(distanceBetweenNegative);
+
+                // Here we check for 3 things that will either let you win, lose or continue shooting.
+                // if = Checks if the user shot more than 500 meters too far - Results in loss
+                // else if = checks if the user has successfully gotten the ball in the cup - result in win
+                // else = makes the loop continue until the user has either succeeded with previous statements or shot too many times.
+
+                if (distanceBetween < -100)
                 {
                     Console.Clear();
-
+                    Console.WriteLine(distanceBetween);
                     swings = swings + 1;
 
-                    Console.WriteLine("You shot too far. You shot " + Math.Round(Math.Abs(distanceBetween),1) + " meters too far in " + swings + " swings.");
+                    Console.WriteLine("You shot too far. You shot " + Math.Round(Math.Abs(distanceBetween), 1) + " meters too far in " + swings + " swings.");
                     Console.ReadKey();
+
                     stayAlive = false;
                 }
-                else if (distanceBetween <= 0.5)
+                else if (distanceBetween < -1 && distanceBetween > -100)
+                {
+                    Console.WriteLine(distanceBetween);
+                    //distanceBetween = Math.Abs(distanceBetween);
+                    //Console.WriteLine(distanceBetween);
+                }
+                else if (Math.Abs(distanceBetween) < 0.1)
                 {
                     Console.Clear();
 
@@ -89,7 +95,8 @@ namespace Assignment_Golf
 
                     for (int i = 0; i < eachSwingDistance.Count; i++)
                     {
-                        Console.WriteLine($"Swing {i + 1} = {eachSwingDistance[i]}");
+                        // This creates the value "Swing X = Y. where X = Swing number and Y = distance on that swing.
+                        Console.WriteLine($"Swing {i + 1} = {Math.Round(eachSwingDistance[i], 1)}");
                     }
                     Console.Write("Press any key to continue, you champion!");
                     Console.ReadKey();
@@ -100,44 +107,86 @@ namespace Assignment_Golf
 
                     swings = swings + 1;
 
-                    if (swings < 10)
+                    if (swings >= 10)
                     {
-                        Console.Write("You suck! Go play Minigolf instead!");
-                    }
-                    Console.Clear();
-                    Console.WriteLine("You have " + Math.Round(Math.Abs(distanceBetween), 1) + " meters to the hole. and you're on " + swings + " swings.");
+                        for (int i = 0; i < eachSwingDistance.Count; i++)
+                        {
+                            Console.WriteLine($"Swing {i + 1} = {Math.Round(eachSwingDistance[i], 1)}");
+                        }
 
+                        Console.WriteLine("\nYou reached the max amount of attempts! You suck!");
+                        Console.ReadKey();
+
+                        stayAlive = false;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine(distanceBetween);
+
+                        Console.WriteLine("You have " + Math.Round(Math.Abs(distanceBetween), 1) + " meters to the hole. and you're on " + swings + " swings.");
+
+                    }
                 }
             }
         }
         static double DistanceToHole()
         {
-            Console.Write("Please enter your wanted distance (in meters) to the hole!");
-            double distanceToHole = double.Parse(Console.ReadLine());
+            Console.Clear();
 
-            if (distanceToHole < 0)
+            Console.Write("Do you wish for the computer to set the distance to the hole? y/n ");
+            string choice = Console.ReadLine();
+            choice = choice.ToLower();
+            if (choice == "y")
             {
-                Console.WriteLine("What kind of backwards dimension are we living in?? Try again!");
+                Random random = new Random();
+                int randomNumber = random.Next(1000, 2000);
+                Console.Clear();
+                Console.WriteLine("The distance to the hole was determined to be: " + randomNumber + " meters. Good luck!");
+                double distanceToHole = Convert.ToDouble(randomNumber);
+
+                return distanceToHole;
+
+            }
+            else if (choice == "n")
+            {
+                Console.Clear();
+
+                Console.Write("Please enter your wanted distance (in meters) to the hole! ");
+                double distanceToHole = double.Parse(Console.ReadLine());
+
+                if (distanceToHole < 0)
+                {
+                    Console.WriteLine("What kind of backwards dimension are we living in?? Try again!");
+                    return DistanceToHole();
+                }
+                else if (distanceToHole > 2000)
+                {
+                    Console.WriteLine("That's some absolute distance! Too big, try again!");
+                    return DistanceToHole();
+                }
+                else
+                {
+                    return distanceToHole;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please try again");
                 return DistanceToHole();
             }
-            else if (distanceToHole >= 2500)
-            {
-                Console.WriteLine("That's some absolute distance! Too big, try again!");
-                return DistanceToHole();
-            }
-            else return distanceToHole;
         }
         static double AllowAngleAmount()
         {
-                                      // <Summary> //
-                     // This Method asks for the angle and verifies the angle
-                     // is between 0 and 89 degrees. if it isn't it'll repeat
-                     // itself until the user gives out a valid amount
+            // <Summary> //
+            // This Method asks for the angle and verifies the angle
+            // is between 0 and 89 degrees. if it isn't it'll repeat
+            // itself until the user gives out a valid amount
 
             Console.Write("Please insert the angle you wish to shoot in: ");
             double angle = double.Parse(Console.ReadLine());
 
-            if (angle < -1 )
+            if (angle < -1)
             {
                 Console.WriteLine("Can't shoot in negative angles!");
                 return AllowAngleAmount();
@@ -157,7 +206,7 @@ namespace Assignment_Golf
             Console.Write("Please insert the velocity you want to shoot in (m/s): ");
             double velocity = double.Parse(Console.ReadLine());
 
-            if (velocity <= 0)
+            if (velocity < 0)
             {
                 Console.WriteLine("You can't shoot in negative speeds, silly you!");
                 return AllowVelocityAmount();
